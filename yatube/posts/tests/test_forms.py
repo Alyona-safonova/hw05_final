ab06c9956076ff691fc3444bb15fd971ee5077cd
Алена Сафонова <safonovaalena2001@gmail.com>
2022-10-28 11:20:37 +0300

    here we go again!

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -1,11 +1,12 @@
 import shutil
 import tempfile
+
 from django.conf import settings
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client, TestCase, override_settings
 from django.urls import reverse
 
-from ..models import Post, Group, User
+from ..models import Group, Post, User
 
 TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,7 +3,6 @@ from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 
 from posts.forms import CommentForm, PostForm
-
 from .models import Comment, Follow, Group, Post, User
 
 MAX_POSTS: int = 10
